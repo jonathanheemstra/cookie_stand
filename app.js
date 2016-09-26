8d638c384ac225d0cfcc6d6accb6f1c524c8c972
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-26 13:26:18 -0700

    add customers per hour reported

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var firstAndPike = {
   avgCookiesPerHour: 6.3,
   location: '1st and Pike',
   hoursOpen: 15,
-  randomCustomersPerHour: function() {
+  findCustomersPerHour: function() {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
-  }
+  },
+  customersPerHourReported: function() {
+    for (var i = 0; i < hoursOpenPerDay.length; i++) {
+      console.log(this.findCustomersPerHour());
+    }
+  },
 };
+
+//random number of customers per hour generated for 1st and Pike
+console.log('1st and Pike random number of customers per hour = ' + firstAndPike.findCustomersPerHour());
