59f2ee1d1de653d5aaa3120b03e9d3ba0b50b837
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-26 13:15:30 -0700

    create random customer per hour method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+var hoursOpenPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var firstAndPike = {
   minCustomersPerHour: 23,
@@ -6,4 +7,7 @@ var firstAndPike = {
   avgCookiesPerHour: 6.3,
   location: '1st and Pike',
   hoursOpen: 15,
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
+  }
 };
