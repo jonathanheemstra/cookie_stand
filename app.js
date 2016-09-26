f76226ef6703519d8e6c6977a03c124d540bb15d
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-26 15:17:59 -0700

    add seatac

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var hoursOpenPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+// 1st and Pike location
 var firstAndPike = {
   minCustomersPerHour: 23,
   maxCustomersPerHour: 65,
@@ -16,23 +17,77 @@ var firstAndPike = {
     for (var i = 0; i < hoursOpenPerDay.length; i++) {
       var randomNumber = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
       this.customersPerHour.push(randomNumber);
-      console.log('Customers per hour at ' + this.location + ' = ' + hoursOpenPerDay[i] + ': ' + randomNumber);
     }
   },
-  totalCookiesSoldPerHour: function() {
+  totalCookiesSoldPerHour: function(){
     for (var i = 0; i < hoursOpenPerDay.length; i++) {
       var cookiesSoldCalc = Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer);
       this.cookiesSoldPerHour.push(cookiesSoldCalc);
       this.totalDailyCookiesSold += cookiesSoldCalc;
-      console.log('Cookies per hour at ' + this.location + ' = ' + hoursOpenPerDay[i] + ': ' + cookiesSoldCalc);
     }
   },
+  calculate: function (){
+    this.customersPerHourReported();
+    this.totalCookiesSoldPerHour();
+  },
 };
-firstAndPike.customersPerHourReported();
-firstAndPike.totalCookiesSoldPerHour();
-var firstAndPikeEl = document.getElementById('firstandpike');
-for (var i = 0; i < firstAndPike.customersPerHour.length; i++) {
-  var liEl = document.createElement('li');
-  liEl.textContent = hoursOpenPerDay[i] + ': ' + firstAndPike.cookiesSoldPerHour[i] + ' cookies';
+firstAndPike.calculate();
+
+function firstAndPikeResults (){
+  var firstAndPikeEl = document.getElementById('firstandpike');
+  for (var i = 0; i < firstAndPike.customersPerHour.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = hoursOpenPerDay[i] + ': ' + firstAndPike.cookiesSoldPerHour[i] + ' cookies';
+    firstAndPikeEl.appendChild(liEl);
+  }
+  firstAndPikeEl = document.getElementById('firstandpike');
+  liEl = document.createElement('li');
+  liEl.textContent = 'Total: ' + firstAndPike.totalDailyCookiesSold + ' cookies';
   firstAndPikeEl.appendChild(liEl);
 }
+firstAndPikeResults();
+
+// SeaTac Airport Location
+var seaTacAirport = {
+  minCustomersPerHour: 3,
+  maxCustomersPerHour: 24,
+  avgCookiesPerCustomer: 1.2,
+  location: '1st and Pike',
+  hoursOpen: 15,
+  customersPerHour: [],
+  cookiesSoldPerHour: [],
+  totalDailyCookiesSold: 0,
+
+  customersPerHourReported: function() {
+    for (var i = 0; i < hoursOpenPerDay.length; i++) {
+      var randomNumber = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour;
+      this.customersPerHour.push(randomNumber);
+    }
+  },
+  totalCookiesSoldPerHour: function(){
+    for (var i = 0; i < hoursOpenPerDay.length; i++) {
+      var cookiesSoldCalc = Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer);
+      this.cookiesSoldPerHour.push(cookiesSoldCalc);
+      this.totalDailyCookiesSold += cookiesSoldCalc;
+    }
+  },
+  calculate: function (){
+    this.customersPerHourReported();
+    this.totalCookiesSoldPerHour();
+  },
+};
+seaTacAirport.calculate();
+
+function seaTacAirportResults (){
+  var seaTacAirportEl = document.getElementById('seatac');
+  for (var i = 0; i < seaTacAirport.customersPerHour.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = hoursOpenPerDay[i] + ': ' + seaTacAirport.cookiesSoldPerHour[i] + ' cookies';
+    seaTacAirportEl.appendChild(liEl);
+  }
+  seaTacAirportEl = document.getElementById('seatac');
+  liEl = document.createElement('li');
+  liEl.textContent = 'Total: ' + seaTacAirport.totalDailyCookiesSold + ' cookies';
+  seaTacAirportEl.appendChild(liEl);
+}
+seaTacAirportResults();
