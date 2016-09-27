ad519d6b6b2725c91fd33929922fdb23a2a06f74
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-27 16:18:18 -0700

    add table body and header

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var hoursOpenPerDay = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var businesses = [];
+var hourlyCookieSales = [];
+var totalAllLocationsSales = [];
 
 //Constructor
 function Stores(minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer, location) {
@@ -23,37 +25,52 @@ function Stores(minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer,
       this.totalDailyCookiesSold += this.cookiesSoldPerHour[i];
     }
   };
-  this.render = function () {
+  this.renderTableBody = function () {
     var tableDataDisplay = document.getElementById('businesses_reporting_js');
-    //Create TH row - includes headers for each hour
-
-    //Create TR for each Business Location - includes name of Location and hourly data
-
-    //Create TD for each Business Location Data
-
+    for (var i = 0; i < businesses.length; i++) {
+      var trEl = document.createElement('tr'); //Create a row for each business location
+      var tdEl = document.createElement('td'); //Create a column for the business location name
+      tdEl.textContent = businesses[i].location; //Add business location name to column
+      trEl.appendChild(tdEl); //Add business location name column to row for current business location[i]
+      for (var a = 0; a <= hoursOpenPerDay.length; a++) { //Create a column for each hour of the day loop
+        tdEl = document.createElement('td'); //Create a column for earch hour of the day
+        tdEl.textContent = businesses[i].cookiesSoldPerHour[a]; //Add hourly sales of cookies per business[i] to hourly column
+        trEl.appendChild(tdEl); //Add hourly sales of cookies per business to column for current business location[i] row
+      }
+      tdEl.textContent = businesses[i].totalDailyCookiesSold; //Create a column for the daily total cookies sold
+      trEl.appendChild(tdEl); //Add daily cookies total sold to business location row
+    }
+    tableDataDisplay.appendChild(trEl); //Add row for each business location to reporting table
   };
-  // this.render = function () {
-  //   var firstAndPikeEl = document.getElementById('firstandpike');
-  //   for (var i = 0; i < this.customersPerHour.length; i++) {
-  //     var liEl = document.createElement('li');
-  //     liEl.textContent = hoursOpenPerDay[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
-  //     firstAndPikeEl.appendChild(liEl);
-  //   }
-  //   liEl = document.createElement('li');
-  //   liEl.textContent = 'Total: ' + this.totalDailyCookiesSold + ' cookies';
-  //   firstAndPikeEl.appendChild(liEl);
-  // };
   this.calculate = function () {
     this.calcCustomersPerHour();
     this.calcCookiesSoldPerHour();
-    // this.render();
+    this.renderTableBody();
   };
   businesses.push(this);
   this.calculate();
 }
 
-var firstAndPike = new Stores(23, 65, 6.3, '1st and Pike');
-var seaTacAirport = new Stores(3, 24, 1.2, 'SeaTac Airport');
-var seattleCenter = new Stores(11, 38, 3.7, 'Seattle Center');
-var capitolHill = new Stores(20, 38, 2.3, 'Capitol Hill');
-var alkiBeach = new Stores(2, 16, 4.6, 'Alki Beach');
+function renderTableHeader() {
+  var tableDataDisplay = document.getElementById('businesses_reporting_js');
+  var trEl = document.createElement('tr'); //Create a row for each business location
+  var thEl = document.createElement('th'); //Create a table header column to hold hours of the day
+  thEl.textContent = ''; //Create a blank space as the first column in the tabel header row
+  trEl.appendChild(thEl); //Add blank space to the table header row
+  for (var i = 0; i < hoursOpenPerDay.length; i++) {
+    thEl = document.createElement('th'); //Create a table header row to hold hours of the day
+    thEl.textContent = hoursOpenPerDay[i]; //Add hour of the day to the table header
+    trEl.appendChild(thEl); //Add hour of the day to the table header row
+  }
+  thEl = document.createElement('th'); //Create a table header row for daily totals
+  thEl.textContent = 'Totals!'; //Add totals to the table header row as last column
+  trEl.appendChild(thEl); //Add totals to the table header row as last column
+  tableDataDisplay.appendChild(trEl); //Add table header row to daily data table
+};
+renderTableHeader();
+
+new Stores(23, 65, 6.3, '1st and Pike');
+new Stores(3, 24, 1.2, 'SeaTac Airport');
+new Stores(11, 38, 3.7, 'Seattle Center');
+new Stores(20, 38, 2.3, 'Capitol Hill');
+new Stores(2, 16, 4.6, 'Alki Beach');
