7d30c652f4681a2807e30c0bd6f3161ec9638d6d
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-27 13:45:50 -0700

    add businesses array to store objects

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var hoursOpenPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var businesses = [];
 
 //Constructor
-function StoresLocations(minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer, location) {
+function Stores(minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer, location) {
   this.minCustomersPerHour = minCustomersPerHour;
   this.maxCustomersPerHour = maxCustomersPerHour;
   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
   this.location = location;
+  this.customersPerHour = [];
+  this.cookiesSoldPerHour = [];
+  this.totalDailyCookiesSold = 0;
   this.calcCustomersPerHour = function() {
     for (var i = 0; i < hoursOpenPerDay.length; i++) {
       this.customersPerHour.push(Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1)) + this.minCustomersPerHour);
@@ -35,10 +39,11 @@ function StoresLocations(minCustomersPerHour, maxCustomersPerHour, avgCookiesPer
     this.calcCookiesSoldPerHour();
     this.render();
   };
+  businesses.push(this);
 }
 
-var firstAndPike = new StoresLocations(23, 65, 6.3, '1st and Pike');
-var seaTacAirport = new StoresLocations(3, 24, 1.2, 'SeaTac Airport');
-var seattleCenter = new StoresLocations(11, 38, 3.7, 'Seattle Center');
-var capitolHill = new StoresLocations(20, 38, 2.3, 'Capitol Hill');
-var alkiBeach = new StoresLocations(2, 16, 4.6, 'Alki Beach');
+var firstAndPike = new Stores(23, 65, 6.3, '1st and Pike');
+var seaTacAirport = new Stores(3, 24, 1.2, 'SeaTac Airport');
+var seattleCenter = new Stores(11, 38, 3.7, 'Seattle Center');
+var capitolHill = new Stores(20, 38, 2.3, 'Capitol Hill');
+var alkiBeach = new Stores(2, 16, 4.6, 'Alki Beach');
